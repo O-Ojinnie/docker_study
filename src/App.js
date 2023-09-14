0e386114632173b5decb5be304ea02d79890b024
O.Ojinnie <o.ojinnie@gmail.com>
2023-09-14 16:47:05 +0900

    init docker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [members, setMembers] = useState([]);
+
+  const memberHandler = () => {
+    axios.get("http://localhost:8081/members").then((res) => {
+      console.log(res.data);
+      setMembers(res.data);
+    });
+  };
+  useEffect(() => {
+    memberHandler();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Members</h1>
+      {members.map((member) => {
+        return (
+          <div>
+            <div>{member.id}</div>
+            <div>{member.name}</div>
+            <div>{member.job}</div>
+            <div>{member.home}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
